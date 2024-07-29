53c47a74189035789efab728e87595377bd80647
SLASRAD <sujeth.lasrado@daimlertruck.com>
2024-07-29 20:32:05 +0530

    first Commit

---
+++ agileprime/src/main/java/com/lazsoft/agileprime/AgileprimeApplication.java
@@ -0,0 +1,13 @@
+package com.lazsoft.agileprime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileprimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileprimeApplication.class, args);
+	}
+
+}
---
+++ agileprime/src/test/java/com/lazsoft/agileprime/AgileprimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazsoft.agileprime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileprimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
