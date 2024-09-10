36a513615b379af7d34dbcfdc4be53d23803e5fd
SLASRAD <sujeth.lasrado@daimlertruck.com>
2024-09-10 17:21:29 +0530

    Added BindingResult

--- agileprime/src/main/java/com/lazsoft/agileprime/AgileprimeApplication.java
+++ agileprime/src/main/java/com/lazsoft/agileprime/AgileprimeApplication.java
@@ -2,8 +2,10 @@ package com.lazsoft.agileprime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.lazsoft.agileprime.*")
 public class AgileprimeApplication {
 
 	public static void main(String[] args) {
---
+++ agileprime/src/main/java/com/lazsoft/agileprime/repositories/ProjectRepository.java
@@ -0,0 +1,8 @@
+package com.lazsoft.agileprime.repositories;
+
+import com.lazsoft.agileprime.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProjectRepository extends CrudRepository<Project,Long> {
+
+}
