1367c9938644f0f99c83523115659721e40c50ad
SLASRAD <sujeth.lasrado@daimlertruck.com>
2024-09-16 18:00:18 +0530

    Complete Backend

--- agileprime/src/main/java/com/lazsoft/agileprime/repositories/ProjectRepository.java
+++ agileprime/src/main/java/com/lazsoft/agileprime/repositories/ProjectRepository.java
@@ -2,7 +2,12 @@ package com.lazsoft.agileprime.repositories;
 
 import com.lazsoft.agileprime.domain.Project;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProjectRepository extends CrudRepository<Project,Long> {
+    Project findByProjectIdentifier(String projectIdentifier);
+    @Override
+    Iterable<Project> findAll();
 
 }
--- agileprime/src/main/java/com/lazsoft/agileprime/services/ProjectService.java
+++ agileprime/src/main/java/com/lazsoft/agileprime/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.lazsoft.agileprime.services;
 
 import com.lazsoft.agileprime.domain.Project;
+import com.lazsoft.agileprime.exceptions.ProjectIdException;
 import com.lazsoft.agileprime.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,7 +12,36 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
-    public Project saveOrUpdateProject(Project project){
-        return projectRepository.save(project);
+    public Project saveOrUpdateProject(Project project) {
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID " + project.getProjectIdentifier().toUpperCase() + " already exists...");
+        }
     }
+
+    public Project findProjectByIdentifier(String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId);
+        if (project != null) {
+            return projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        } else {
+            throw new ProjectIdException("Project ID" + projectId.toUpperCase() + " does not exists...");
+        }
+
+    }
+
+    public Iterable<Project> findAllProjects() {
+        return projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier(String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if (project == null) {
+            throw new ProjectIdException("Project ID" + projectId.toUpperCase() + " was not found...");
+        }
+        projectRepository.delete(project);
+
+    }
+    
 }
